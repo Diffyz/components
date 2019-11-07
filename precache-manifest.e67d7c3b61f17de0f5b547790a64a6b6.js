cf4b7810e4728c6629d78cc5e42076669ef98417
EgorPash <e.pashko@itransition.com>
2019-11-07 17:37:42 +0300

    Updates

---
+++ precache-manifest.e67d7c3b61f17de0f5b547790a64a6b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4ebb97005d3dafec43992586ce9b7d",
+    "url": "/components/index.html"
+  },
+  {
+    "revision": "22baafbe786e45783bc1",
+    "url": "/components/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "22baafbe786e45783bc1",
+    "url": "/components/static/js/2.0884cf0c.chunk.js"
+  },
+  {
+    "revision": "421f6bd0d09cffa8ae56",
+    "url": "/components/static/js/main.78ba8adc.chunk.js"
+  },
+  {
+    "revision": "725123d02c3338fa28be",
+    "url": "/components/static/js/runtime-main.6af327e5.js"
+  },
+  {
+    "revision": "acdffed4e29ec6df1a2dcacf7fc408c0",
+    "url": "/components/static/media/modal-window-cross.acdffed4.svg"
+  }
+]);
